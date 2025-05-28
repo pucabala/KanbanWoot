97b3eb50a1fb2c37b4c4096971c5dd093d12fda8
pucabala <publiomelo@gmail.com>
2025-05-28 01:58:05 +0000

    Reset Chatwoot environment variables to default values

--- public/.env.js
+++ public/.env.js
@@ -1,8 +1,8 @@
 //.env.js
 // Este arquivo substitui o uso do .env para variáveis de ambiente no frontend
 window._env_ = {
-  REACT_APP_CHATWOOT_TOKEN: 'xxxxxxzxxxxxxxxxxxxxx',
-  REACT_APP_CHATWOOT_ACCOUNT_ID: '1',
-  REACT_APP_CHATWOOT_URL: 'https://xxxxxxxxxxx.com',
+  REACT_APP_CHATWOOT_TOKEN: '',
+  REACT_APP_CHATWOOT_ACCOUNT_ID: '',
+  REACT_APP_CHATWOOT_URL: '',
   REACT_APP_DEBUG: 'true',
 };
